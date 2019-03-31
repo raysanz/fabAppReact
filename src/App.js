30768a16fe256a9f8b9769919462bfc130205693
raysanz <rudysanchez93@gmail.com>
2019-03-31 13:18:59 -0700

    added mdbreact dependency for carousel // added carousel component // carousel on home

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import logo from './logo-fabkids.png';
 import './App.css';
 import Navigation from './Components/navigation';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import CarouselPage from "./Components/carousel"
 
 
 const Home = () => (
   <div>
-    Home
+    <div class="offerBar container">
+      <span class="thin">NEW VIP OFFER</span>
+      <span class="thick">50% off Site-Wide</span>
+    </div>
+    <CarouselPage />
   </div>
 )
 
